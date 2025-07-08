69a4698c43678563ddb7ca913f24933861523b54
FaseehZafar <faseehzafar.dev@gmail.com>
2025-07-08 16:23:34 +0500

    Completed q1 of lab2

--- Solutions/Lab_2/q1.cpp
+++ Solutions/Lab_2/q1.cpp
@@ -1,2 +1,26 @@
-we will be coding it soon...
+#include <iostream>
+using namespace std;
+int main() {
 
+    int a,b,*ptr_a,*ptr_b;
+    
+    cout << "Enter first Integer: "<<endl;
+    cin >> a;
+    
+    cout << "Enter Second Integer: "<<endl;
+    cin >> b;
+    
+    ptr_a=&a;
+    ptr_b=&b;
+    
+    cout << "Value of a: "<<*ptr_a<<endl;
+    cout << "Value of b: "<<*ptr_b<<endl;
+    
+    cout << "Address of a: "<<ptr_a<<endl;
+    cout << "Address of b: "<<ptr_b<<endl;
+    
+    
+
+
+    return 0;
+}
